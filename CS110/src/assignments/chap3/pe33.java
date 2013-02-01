ebecd55c103624814fcce4cf45baad675b22d0e6
saron <saron@140.190.156.136>
2013-02-01 17:41:01 -0600

    secon commit

--- CS110/src/assignments/chap3/pe33.java
+++ CS110/src/assignments/chap3/pe33.java
@@ -5,16 +5,10 @@ import java.util.Scanner;
 public class pe33 {
 
 	public static void main(String[] args) {
-		Double a;
-		Double b;
-		Double c;
-		Double d;
-		Double e;
-		Double f;
-		Double x;
-		Double y;
+		Double a, b, c, d, e, f;
+		Double x, y;
 		Double denominator;
-		System.out.println("Enter a, b, c, d, e, f: ");
+		System.out.print("Enter a, b, c, d, e, f: ");
 		Scanner scanner = new Scanner(System.in);
 		a = scanner.nextDouble();
 		b = scanner.nextDouble();
@@ -26,7 +20,7 @@ public class pe33 {
 		if (denominator != 0) {
 			x = (e * d - b * f)/denominator;
 			y = (a * f - e * c)/denominator;
-			System.out.println("X is " + x + " and Y is " + y);
+			System.out.printf("X is %4.3f and Y is %4.3f", x, y);
 		}
 		else {
 			System.out.println("The equation has no solution");
