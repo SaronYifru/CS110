55a852c8192275eef78366f58b6e88d5794266d2
saron <saron@140.190.155.38>
2013-02-26 08:28:14 -0600

    final commit

--- CS110/src/assignments/chap5/MyTriangle.java
+++ CS110/src/assignments/chap5/MyTriangle.java
@@ -20,8 +20,8 @@ public class MyTriangle {
 	}
 
 	public static boolean isValid(double side1, double side2, double side3) {
-		if (side1 + side2 <= side3 || side2 + side3 <= side1
-				|| side1 + side3 <= side2) {
+		if ((side1 + side2 <= side3) || (side2 + side3 <= side1)
+				|| (side1 + side3 <= side2)) {
 			return false;
 		}
 		return true;
