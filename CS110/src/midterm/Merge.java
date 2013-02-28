9db9464fdb50392a77442bdc5662f268e7b1b3be
saron <saron@140.190.155.182>
2013-02-28 09:38:42 -0600

    final

--- CS110/src/midterm/Merge.java
+++ CS110/src/midterm/Merge.java
@@ -7,40 +7,52 @@ public class Merge {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter list1: ");
 		int elementsinList1 = scanner.nextInt();
+		System.out.print("Enter list2: ");
 		int elementsinList2 = scanner.nextInt();
 		int[] list1 = new int[elementsinList1];
 		int[] list2 = new int[elementsinList2];
-		for (int i, j = 0; i < elementsinList1; i++) {
+		for (int i = 0; i < elementsinList1; i++) {
 			list1[i] = scanner.nextInt();
 			
 		}
 		for (int j = 0; j < elementsinList2; j++) {
 			list2[j] = scanner.nextInt();
 		}
+		System.out.println(merge(list1, list2));
 		
 
 	}
 	public static int[] merge(int[] list1, int[] list2) {
 		int[] mergedList = new int[list1.length + list2.length];
-		int max = 0;
-		for (int i = 0; i < list1.length; i++) {
-			for (int j = 0; j < list2.length; j++){
-				for (int k = list2.length + list1.length - 1; k >= 0; k--){
-					if ((list1[i] > list2[i])){
-						mergedList[k] = list1[i];
-						max = list1[i];
+		int length = list1.length + list2.length;
+		
+		
+		for ( int i = 0, j = 0, k = 0; i < list1.length && j < list2.length && k < length; i++, j++, k++) {
+			if (list1[i] < list2[j]) {
+				mergedList[k] = list1[i];
+				
 						
 				}
-					
-					
-					
-					
-					
-				}
+			else {
+				mergedList[k] = list2[j];
 			}
+			
+		}
+		for ( int i = 0, k = 0; i < list1.length && k < length; i++, k++) {
+			
+				mergedList[k] = list1[i];	
+					
+					
+					
+		}
+		for ( int j = 0, k = 0; j < list2.length && k < length; j++, k++) {
+			mergedList[k] = list2[j];
+		}
+		return mergedList;
 		}
 		
 	}
 
-}
+
