c4396628071dbb3a8a8ffc0223dd8a8c86faa1cc
saron <saron@140.190.155.94>
2013-04-07 17:27:41 -0500

    second commit

--- CS110/src/assignments/chap9/HangMan.java
+++ CS110/src/assignments/chap9/HangMan.java
@@ -3,49 +3,74 @@ package assignments.chap9;
 import java.util.Random;
 
 public class HangMan {
-	private final String[] words = {"java", "program", "write", "awesome", "computer"};
-	private char letterGuess;
+	private final String[] words = { "java", "program", "write", "awesome",
+			"computer" };
 	private String wordWithAsteriks;
 	private String randomWord;
+	private int numberOfMissed;
 	
-    public HangMan() {
-    	Random random = new Random();
-    	int randomIndex = random.nextInt(words.length);
-    	randomWord = words[randomIndex];
-    	
-    	
-    }
-    public String replaceByAsteriks(){
-    	wordWithAsteriks = randomWord;
-    	for (int i = 0; i < randomWord.length(); i++) {
-    		wordWithAsteriks = wordWithAsteriks.replace(wordWithAsteriks.charAt(i), '*');
-    		
-    	}
-    	System.out.println(wordWithAsteriks);
-    	return wordWithAsteriks;
-    }
-    public String findAndDisplay(char letterGuess) {
-    	if (wordWithAsteriks.indexOf(letterGuess)!= -1) {
-    		return null;
-    	}
-    	int index = randomWord.indexOf(letterGuess);
-    	if (index != -1) {
-    		return wordWithAsteriks;
-    	}
-    	
-    	wordWithAsteriks = wordWithAsteriks.replace(wordWithAsteriks.charAt(index), letterGuess);
-    	return wordWithAsteriks;
-    		
-    }
-    public void printStatusAndPrompt() {
-    	if (findAndDisplay(letterGuess) == null) {
-    		System.out.println(letterGuess + " is already in the word");
-    	}
-    	System.out.println("(Guess) Enter a letter in word " + wordWithAsteriks + " > ");
-    	
-    }
-    public void firstPrompt() {
-		System.out.println("(Guess) Enter a letter in word " + replaceByAsteriks() + " > ");
+
+	public HangMan() {
+		Random random = new Random();
+		int randomIndex = random.nextInt(words.length);
+		randomWord = words[randomIndex];
+		
+
 	}
-    
+
+	public String replaceByAsteriks() {
+		wordWithAsteriks = randomWord;
+		for (int i = 0; i < randomWord.length(); i++) {
+			wordWithAsteriks = wordWithAsteriks.replace(
+					wordWithAsteriks.charAt(i), '*');
+
+		}
+
+		return wordWithAsteriks;
+	}
+
+	public boolean isletterInWord(char letterGuess) {
+		// check if the letter is already found
+		if (wordWithAsteriks.indexOf(letterGuess) != -1) {
+			System.out.println(letterGuess + " is already in word");
+			numberOfMissed++;
+			return false;// the letter is already found
+		}
+		int index = randomWord.indexOf(letterGuess);
+		if (index == -1) {
+			numberOfMissed++;
+			return false;// the letter is not in word
+
+		}
+		for (int i = 0; i < randomWord.length(); i++) {
+			index = randomWord.indexOf(letterGuess, i);
+			if (index != -1) {
+				char[] arrayOfAsteriks = wordWithAsteriks.toCharArray();
+				arrayOfAsteriks[index] = letterGuess;
+				wordWithAsteriks = String.valueOf(arrayOfAsteriks);
+			}
+		}
+
+		
+		return true;
+	}
+
+	public void printStatusAndPrompt() {
+		System.out.print("(Guess) Enter a letter in word " + wordWithAsteriks
+				+ " > ");
+
+	}
+
+	public boolean continueGame() {
+		if (wordWithAsteriks.indexOf('*') != -1) {
+			return true;
+		}
+		return false;
+
+	}
+
+	public void endOfGameDetails() {
+		System.out.println("The word is " + randomWord + " . You missed "
+				+ numberOfMissed + " time");
+	}
 }
--- CS110/src/assignments/chap9/Pe925.java
+++ CS110/src/assignments/chap9/Pe925.java
@@ -6,14 +6,19 @@ public class Pe925 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		HangMan hangMan1 = new HangMan();
-		hangMan1.firstPrompt();
-		for (int i = 0; i < 10; i++) {
-			char letterGuess = scanner.next().charAt(0);
+		String status = "y";
+		while (status.equals("y")) {
+			HangMan hangMan1 = new HangMan();
 			hangMan1.replaceByAsteriks();
-			hangMan1.findAndDisplay(letterGuess);
-			hangMan1.printStatusAndPrompt();
-			
+			while (hangMan1.continueGame()) {
+				hangMan1.printStatusAndPrompt();
+				char letterGuess = scanner.next().charAt(0);
+				hangMan1.isletterInWord(letterGuess);
+			}
+			hangMan1.endOfGameDetails();
+			System.out
+					.print("Do you want to start another game? Enter y or n > ");
+			status = scanner.next();
 		}
 
 	}
